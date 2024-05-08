57ba01494baabfa4f7865482e72a08a523775155
Fabrice <f.nkurunziz@alustudent.com>
2024-05-08 11:32:35 +0200

    defined variables with their type annotations

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -4,3 +4,9 @@
     This module contains the variables that are defined
     with specifying their data types
 """
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
