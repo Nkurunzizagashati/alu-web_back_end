ee4790c0b7408ef55abc831463b3b520200892b5
Fabrice <f.nkurunziz@alustudent.com>
2024-05-08 10:35:09 +0200

    added a function that will take a float number and return its floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-import math
-
 """
     This module contains functions related to mathematical operations.
+"""
+
+import math
+"""
+    Imported the math module and it's the one that will
+    give us access to the floor method
 """
 
 
