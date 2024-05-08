56c8a30cb974112a32c3e14ca336273d86432436
Fabrice <f.nkurunziz@alustudent.com>
2024-05-08 19:06:24 +0200

    added the function and documented it

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+"""
+    This module contains a function that takes in
+    a string and an int or a float and returns a tuple
+"""
+
+from typing import Union
+"""
+    Imported Union from typing for defining hte possibility
+    of two different data types
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """
+        This function takes in a string and an int or a
+        float and return a tuple with the first element
+        which is that string and the second which is the
+        square of that int or float depending on the one
+        used
+    """
+
+    return tuple[k, v * v]
