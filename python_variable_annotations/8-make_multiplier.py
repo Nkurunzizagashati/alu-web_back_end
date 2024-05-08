57612759bbc66dbc25b3ddda8eba6d8c40f3e0b1
Fabrice <f.nkurunziz@alustudent.com>
2024-05-08 19:27:59 +0200

    added a function that returns a collable

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -14,7 +14,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-    This function takes a float multiplier as an argument and returns a function
+    This function takes a float multiplier as an argument and returns
+    a function
     that multiplies a float by the multiplier.
     """
     def multiplier_function(value: float) -> float:
