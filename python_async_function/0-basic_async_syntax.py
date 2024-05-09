d7498776b7be007954103eea01ef23dd24944f9f
Fabrice <f.nkurunziz@alustudent.com>
2024-05-09 21:24:39 +0200

    documented the module

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+"""
+    This module contains a coroutine that takes an int
+    argument(max_delay) with a default value of 10. It waits for
+    a random delay between 0 to max_delay seconds and eventually
+    returns it.
+"""
