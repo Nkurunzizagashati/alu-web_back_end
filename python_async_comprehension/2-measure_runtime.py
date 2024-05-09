c6d2c2c4ada44b9f2061e53de29fc0b61c53ec03
Fabrice <f.nkurunziz@alustudent.com>
2024-05-09 20:48:08 +0200

    documented the module

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,9 @@
 #!/usr/bin/node python3
+
+"""
+    This module contains a coroutine that imports async_comprehension
+    from the 1-async_comprehension.py. It will execute
+    async_comprehension four times in parallel using asyncio.gather.
+
+    The coroutine should measure the total runtime and return it
+"""
