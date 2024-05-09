71db7cf9439fabe47e7395b258e31700f4dbb20c
Fabrice <f.nkurunziz@alustudent.com>
2024-05-09 21:33:32 +0200

    added a coroutine that takes in an int which is a max_delay and it will wait for a random between 0 and max_delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,3 +6,17 @@
     a random delay between 0 to max_delay seconds and eventually
     returns it.
 """
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """
+        This function takes in a max_delay, and int argument
+        and wait for a random delay between 0 and max_delay
+        and returns the max_delay.
+    """
+
+    await asyncio.sleep(random.uniform(0, max_delay))
+    return max_delay
