764f60411507b2ecc744da4269e8ff1335920470
Fabrice <f.nkurunziz@alustudent.com>
2024-05-09 21:52:26 +0200

    documented the module

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+    This module contains an async routine called wait_n,
+    it takes in 2 int arguments n, and max_delay,
+    it will spawn wait_random n times with the specified max_delay.
+    wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort() because of concurrency
+"""
