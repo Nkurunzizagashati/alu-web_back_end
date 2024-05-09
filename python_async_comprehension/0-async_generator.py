b648ecb07e53de9ad68322433d86c1e31d92ebab
Fabrice <f.nkurunziz@alustudent.com>
2024-05-09 20:02:09 +0200

    documented the module

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+"""
+    This module contains a coroutine that takes no
+    argument. It loops 10 times, each time asynchronously wait
+    1 second, then yield a random number between 0 and 10.
+    I am using random module
+"""
