62f0d3798fcce2df30cd4224572ae8f0f8bbc0cd
Fabrice <f.nkurunziz@alustudent.com>
2024-05-09 20:28:58 +0200

    documented the module

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+"""
+    This module contains a coroutine that takes no
+    argument. The coroutine will collect 10 random
+    numbers using an async comprehensing over, then
+    return the 10 random numbers
+
+    I am also importing async_generato from
+    0-async_generator.py
+"""
