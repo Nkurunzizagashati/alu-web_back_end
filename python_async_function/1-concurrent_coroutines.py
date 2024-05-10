482a8099003b4b434122c39616a223ccc52ceea1
Fabrice <f.nkurunziz@alustudent.com>
2024-05-10 10:04:54 +0200

    added a coroutine that takes in an int which is a max_delay and it will wait for a random between 0 and max_delay

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,10 +11,11 @@
 
 import asyncio
 from typing import List
+from typing import Union
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[Union[float, int]]:
     """
         This async routine takes in two int arguments and
         returns a list of all the delays
