e0e2d1c16672e0ca1290273511d2baef00db8852
Fabrice <f.nkurunziz@alustudent.com>
2024-05-10 11:05:05 +0200

    documented the module

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+"""
+    This module contains a with two arguments n and max_delay
+    which are both integers. It measures the total execution
+    time for wait_n(n, max_delay) and returns the total_time/n
+    which should be a float.
+"""
