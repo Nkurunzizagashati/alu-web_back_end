de29a3a35c99b8282d2306ced3159f5ba878af2c
Fabrice <f.nkurunziz@alustudent.com>
2024-05-10 10:50:17 +0200

    created the async routine that returns a list of all the delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -21,5 +21,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
 
     tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
-    result = await asyncio.gather(*tasks)
+    # result = await asyncio.gather(*tasks)
     return [await task for task in asyncio.as_completed(tasks)]
