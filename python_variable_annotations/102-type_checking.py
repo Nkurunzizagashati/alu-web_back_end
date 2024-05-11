a192051cf7eb8b661d1416fb8f11c6bd8b684c4d
Fabrice <f.nkurunziz@alustudent.com>
2024-05-11 09:50:49 +0200

    added the annotation

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+"""
+
+from typing import Tuple, Sequence, Any
+
+def zoom_array(lst: Sequence[Any], factor: int = 2) -> Tuple[Any, ...]:
+    zoomed_in: Tuple[Any, ...] = tuple(
+        item for item in lst
+        for i in range(factor)
+    )
+    return zoomed_in
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)  # Corrected factor value to integer
