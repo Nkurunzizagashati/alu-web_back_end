79a188bea855a3456f580697b1907fe4842c2557
Fabrice <f.nkurunziz@alustudent.com>
2024-05-11 09:26:05 +0200

    added the correct type annotations to the function params

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+    I am addin the correct type annotation
+    to the function's params
+"""
+
+from typing import TypeVar, Dict, Any
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Dict[Any, T], key: Any, default: T = None) -> T:
+    """
+        Added the correct type annotation to this function
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
