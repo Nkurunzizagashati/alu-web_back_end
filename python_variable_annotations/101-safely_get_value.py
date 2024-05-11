aac25869e0832be0fc9b10d02dcbdae482af8a9e
Fabrice <f.nkurunziz@alustudent.com>
2024-05-11 10:12:56 +0200

    added the annotation

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,12 @@ from typing import TypeVar, Mapping, Any, Union
 
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+        I am addin the correct type annotation
+        to the function's params
+    """
     if key in dct:
         return dct[key]
     else:
