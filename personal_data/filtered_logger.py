e6282d9fb3757fe7b7cd17db5dbee080b98892d5
Fabrice <f.nkurunziz@alustudent.com>
2024-05-24 16:56:01 +0200

    added the function that returns the log message obfuscated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -22,5 +22,5 @@ def filter_datum(fields: List[str], redaction: str,
         is separating all fields in the log line.
     """
     pattern = '|'.join([f'{field}=[^ {separator}]+' for field in fields])
-    return re.sub(pattern, lambda m: f'{m.group().split("=")[0]}={redaction}'
-                , message)
+    return re.sub(pattern, lambda m: f'{m.group().split("=")[0]}={redaction}',
+                  message)
